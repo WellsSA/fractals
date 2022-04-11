b4efb94cbc9ee113a061605b0e2060059123332e
WellsSA <wel.cavzod@gmail.com>
2022-04-11 06:33:07 -0300

    feat(): main script and styles

---
+++ fractal_trees/src/main-script.js
@@ -0,0 +1,19 @@
+import { Canvas, Line, Fractal } from './entity/index.js';
+import THEMES from './config/themes.js';
+import { $ } from './util/jquery.js';
+
+const canvasElement = $('#root canvas');
+
+const canvas = new Canvas(canvasElement);
+
+const drawLine = ([initialX, initialY], thickness, length, angle = 45) => {
+  const _line = new Line([initialX, initialY], thickness, length, angle);
+
+  canvas.drawLine(_line, THEMES.RED);
+
+  return _line.finalCoordinates;
+};
+
+const fractal = new Fractal({ drawingInterface: drawLine });
+
+fractal.startDrawing();
